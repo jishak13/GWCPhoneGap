18017569f3e9ce6701be6a8fe3120128e7d62fdb
Joseph <jishak1@asu.edu>
2018-03-29 08:55:47 -0700

    Fixing up Index.html

--- www/js/index.js
+++ www/js/index.js
@@ -8,7 +8,6 @@ var app = {
     },
 
     // deviceready Event Handler
-    //
     // Bind any cordova events here. Common events are:
     // 'pause', 'resume', etc.
     onDeviceReady: function() {
@@ -17,12 +16,11 @@ var app = {
         // alert("Device is ready");
         if(navigator.onLine) {
             console.log("This CAT is Online!")
-            console.log("The ID of the first object: " + todos.ID);
-            console.log("The Description of the first object: " + todos.Description);
+            // console.log("The ID of the first object: " + todos.ID);
+            // console.log("The Description of the first object: " + todos.Description);
             /** If this is online, the app should call the api to get the data */
-            $('#todoid').val("The ID of the first object: " + todos.ID);
-            
-
+            // $('#todoid').val("The ID of the first object: " + todos.ID);
+          
         }else {
             console.log("This CAT is Offline!")
             console.log("The ID of the first object: " + todos.ID);
@@ -36,10 +34,25 @@ var app = {
 app.initialize();
 
 $('#btn1').on('click',()=>{
+    let response = undefined;
+
+    $.ajax({
+        type: 'GET',
+        url: 'http://localhost:1337/hello',
+        crossDomain: true,
+        dataType: 'json',
+        success: function (responseData, textStatus, jqXHR) {
+           console.log(responseData);
+           response = responseData;
+        },
+        error: function (responseData, textStatus, errorThrown) {
+            console.log(responseData);
+            console.log(textStatus);
+            console.log(errorThrown);
+        }
+    });
     //Selects the todoid element and appends the string of texts and values from the js/json/todos.js array
-    $('#todoid').append("The ID of the first object: " + todos[0].ID +
-    "<br> The Description of the first object: " +
-    todos[0].Description);
+    $('#todoid').append(JSON.stringify(response));
 });
 
 $('#btn2').on('click', ()=> {
@@ -54,9 +67,26 @@ $('#btn2').on('click', ()=> {
 
 
     let newTodo = {ID: newID, Description: newDescription};
-    todos.push(newTodo);
-    console.log(todos);
-    document.write(JSON.stringify(todos));
+
+    $.ajax({
+        type: 'POST',
+        url: 'http://localhost:1337/sayhello',
+        crossDomain: true,
+        data: newTodo,
+        dataType: 'json',
+        success: function (responseData, textStatus, jqXHR) {
+           console.log("The response was send:");
+           console.log(responseData);
+        },
+        error: function (responseData, textStatus, errorThrown) {
+            console.log(responseData);
+            console.log(textStatus);
+            console.log(errorThrown);
+        }
+    });
+    // todos.push(newTodo);
+    // console.log(todos);
+    // document.write(JSON.stringify(todos));
 
 });
 
@@ -67,18 +97,33 @@ function onDeviceReady() {
     if(navigator.onLine) {
         console.log("This CAT is Online!")
         console.log(todos);
-        console.log("The ID of the second object: " + todos[1].ID);
-        console.log("The Description of the second object: " + todos[1].Description);
+        // console.log("The ID of the second object: " + todos[1].ID);
+        // console.log("The Description of the second object: " + todos[1].Description);
 
         /** If this is online, the app should call the api to get the data */
      
+        $.ajax({
+            type: 'GET',
+            url: 'http://localhost:1337/hello',
+            crossDomain: true,
+            dataType: 'json',
+            success: function (responseData, textStatus, jqXHR) {
+               console.log("The response was send:");
+               console.log(responseData);
+            },
+            error: function (responseData, textStatus, errorThrown) {
+                console.log(responseData);
+                console.log(textStatus);
+                console.log(errorThrown);
+            }
+        });
 
         
 
     }else {
         console.log("This CAT is Offline!")
-        console.log("The ID of the first object: " + todos[0].ID);
-        console.log("The Description of the first object: " + todos[0].Description);  
+        // console.log("The ID of the first object: " + todos[0].ID);
+        // console.log("The Description of the first object: " + todos[0].Description);  
     }
 
 }
--- www/js/json/todos.js
+++ www/js/json/todos.js
@@ -1,4 +1,3 @@
 var todos = [
-    { ID: '1', Description: 'Take out garbage'},
-    { ID: '2', Description: 'Laundry'}
+ 
 ];
